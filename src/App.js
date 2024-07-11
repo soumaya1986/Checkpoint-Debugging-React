cfa48f3a41d0f1f62eda4634143335fcb1741bfb
soumaya1986 <soumaya.chamekh@yahoo.com>
2024-07-11 14:28:38 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Counter from "./Counter";
+import Greeting from "./Greeting";
+
+function App() {
+  return (
+    <div>
+      <Greeting name="John" />
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Counter.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  return (
+    <div>
+      <p>Count: {count}</p>
+      <button onClick={increment}>Increment</button>
+    </div>
+  );
+};
+// Counter.js
+const increment = () => {
+  setCount(count + 1);
+};
+
+export default Counter;
